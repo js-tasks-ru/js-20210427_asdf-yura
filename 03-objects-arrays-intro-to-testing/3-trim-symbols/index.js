3a0484567cadae3a7a3f7a0eea62535011e33725
Urii Frolkin <asdf_yura@mail.ru>
2021-05-06 10:33:24 +0200

    Objects and arrays, testing basics

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,31 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    if(isNaN(size) )  
+            return string;
+        
+    if (size==0) 
+            return "";
 
+    let charpre = "";
+    let count = 0;
+    let newString="";
+    for(let char of string )
+    {
+        if(char === charpre)
+        {
+            count++;
+            if(count<size)
+            {
+                newString += char;
+            }
+        }
+        else
+        {
+            newString += char;
+            charpre = char;
+            count = 0;
+        }
+    }
+    return newString;
 }
