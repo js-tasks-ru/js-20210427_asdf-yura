ffbbea1cbf81b3c0beb459e92990eeebcc1ac2da
Urii Frolkin <asdf_yura@mail.ru>
2021-05-15 02:36:58 +0200

    1-column-chart. Section 4

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,22 +1,38 @@
 ﻿export default class ColumnChart {
+    
+    element = null;
+    subElements = {};
+    chartHeight = 50;
 
-    constructor(obj) {
-        this.element;
-        this.chartHeight = 50;
-        if (obj) {
-            this.data = obj.data;
-            this.label = obj.label;
-            this.value = obj.value;
-            this.link = obj.link;
-            if (obj.formatHeading) {
-                this.value = this.formatHeading(this.value);
-             }
-        }
-        this.render();
-        this.initEventListeners();
+    constructor ({
+        data = [],
+        label = '',
+        value = 0,
+        link = '',
+        formatHeading = (str) => { return str }
+        } = {})  {
+
+            this.data = data;
+            this.label = label;
+            this.value = value;
+            this.link = link;
+            this.formatHeading = formatHeading;
+            this.value = this.formatHeading(this.value);  //fn=()=>{}
+            this.render();
     }
 
-    formatHeading(dat) { return `USD ${dat}`; }
+    //formatHeading(dat) { return `USD ${dat}`; }
+
+     getColumnBody(data) {
+        
+        const maxValue = Math.max(...data);
+        const scale = this.chartHeight / maxValue;
+
+        return data.map(item => {
+             const percent = (item / maxValue * 100).toFixed(0);
+             return ` <div style="--value: ${Math.floor(item * scale)}" data-tooltip="${percent}%"></div> `;
+        }).join('');
+    }
 
     getColumnProps(data) {
         
@@ -30,57 +46,65 @@
         });
     }
 
-    render() {
-        this.element = document.createElement('div'); // (*)
-
-        let stringSource;
-        if (!this.data || this.data.length === 0) {
-            stringSource += ` 
-            <div class="column-chart column-chart_loading" style="--chart-height: 50">
-            `;
-        } else { 
-            stringSource += `
-            <div class="column-chart" style="--chart-height: 50"> 
-            ` 
-        };
-        stringSource += `
-        <div class="column-chart__title">
-        ${this.label}
-        `;
-        if (this.link) {
-            stringSource += `
-            <a href="${this.link}" class="column-chart__link">View all</a>
-            </div> `;
-        } else { stringSource += `</div>`; }
-          
-
-        stringSource += `
-        <div class="column-chart__container">
-        <div data-element="header" class="column-chart__header">${this.value}</div>
-        <div data-element="body" class="column-chart__chart"> 
-        `;
-
-        if (this.data && Array.isArray(this.data)) {
-            const proportion = Array.from(this.getColumnProps(this.data))   
-            if (this.data && this.data.length > 0 ) {
-                for (let i = 0; i < this.data.length; i++) {
-                    stringSource +=`<div style="--value: ${proportion[i].value}" data-tooltip="${proportion[i].percent}"></div> `;
-                }
-            }
-        }
-            
-        stringSource +=  `
+    get template () {
+        return `
+        <div class="column-chart column-chart_loading" style="--chart-height: 50">
+            <div class="column-chart__title">
+            ${this.label} ${this.getLink()}
+            </div>
+            <div class="column-chart__container">
+                ${this.value}
+                <div data-element="header" class="column-chart__header">${this.value}</div>
+                <div data-element="body" class="column-chart__chart"> 
+                ${this.getColumnBody(this.data)}
                 </div>
             </div>
-        </div> `; 
+        </div>
+        `;
+    }
 
-        this.element.innerHTML = stringSource;
+    getSubElements(element) {
+        const elements = element.querySelectorAll('[data-element]');
+        return [...elements].reduce((accum,subElement) => {
+            accum[subElement.dataset.element] = subElement;
+            return accum;
+        }, {});
+    }
+
+    getLink() {
+        return this.link ? `<a href="${this.link}" class="column-chart__link">View all</a>` : '';
+    }
+
+    
+
+    render() {
+        this.element = document.createElement('div'); // (*)
+        this.element.innerHTML = this.template;
+        
         // NOTE: в этой строке мы избавляемся от обертки-пустышки в виде `div`
         // который мы создали на строке (*)
-       this.element = this.element.firstElementChild;
-        
+        this.element = this.element.firstElementChild;
+
+        this.showLoading();
+        this.subElements = this.getSubElements(this.element);
+
    }
 
+   showLoading() {
+        if (this.data.length) {
+            this.element.classList.remove('column-chart_loading');
+        }
+   }
+
+   update(data) {
+        if (Array.isArray(data) && data.length>0) {
+                this.data = data;
+                this.showLoading();
+                this.subElements.body.innerHTML = this.getColumnBody(data);
+        }
+   }
+
+       
 
   initEventListeners () {
     // NOTE: в данном методе добавляем обработчики событий, если они есть
@@ -91,21 +115,14 @@
     this.element.remove();
   }
 
-
+  // NOTE: удаляем обработчики событий, если они есть
   destroy() {
     this.remove();
-    // NOTE: удаляем обработчики событий, если они есть
+    this.element = null;
+    this.subElements = {};
   }
 
-  update(data) {
-         this.data = data;
-         try {
-             const div = document.querySelector(`.dashboard__chart_${this.label}`);
-             div.firstElementChild.remove(); 
-             this.render();
-             div.append(this.element);
-         } catch {}
-  }
+  
 }
 
 
