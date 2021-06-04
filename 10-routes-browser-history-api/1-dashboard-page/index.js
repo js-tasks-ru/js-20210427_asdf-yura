b183fe4e5919454f295c805532a3ff63b5ff2976
Urii Frolkin <asdf_yura@mail.ru>
2021-06-04 13:08:19 +0200

    1-dashboard-page

--- 10-routes-browser-history-api/1-dashboard-page/index.js
+++ 10-routes-browser-history-api/1-dashboard-page/index.js
@@ -8,5 +8,119 @@ import fetchJson from './utils/fetch-json.js';
 const BACKEND_URL = 'https://course-js.javascript.ru/';
 
 export default class Page {
+    element;
+    subElements = {};
+    components = {};
+
+    constructor() {
+    }
+
+    async updateComponents(from, to) {
+        const data = await fetchJson(`${BACKEND_URL}/api/dashboard/bestsellers`)
+        this.components.SortableTable.addRows(data);
+
+        this.components.ordersChart.update(from,to);
+        this.components.salesChart.update(from,to);
+        this.components.customsChart.update(from,to);
+    }
+
+    initComponents() {
+        const now = new Date();
+        const from = new Date();
+        const to = new Date( now.setMonth( now.getMonth() - 1 ) );
+        this.components.RangePicker = new RangePicker( {from, to } );
+
+        this.components.SortableTable = new SortableTable(header, {
+            url: `api/dashboard/bestsellers`,  
+            isSortlocaly: true
+        });
+
+        this.components.ordersChart = new ColumnChart( {
+            url: `api/dashroad/orders`,
+            range: { from, to },
+            label: 'orders'
+        });
+        this.components.salesChart = new ColumnChart( {
+            url: `api/dashroad/sales`,
+            range: { from, to },
+            label: 'orders'
+        });
+
+       this.components.customersChart = new ColumnChart( {
+            url: `api/dashroad/customers`,
+            range: { from, to },
+            label: 'orders'
+        });
+       
+    }
+
+    randerComponents() {
+        Object.keys(this.components).forEach(component => {
+            const { root } = this.subElements[component];
+            const { element } = this.components[component];
+        });
+    }
+
+    get template() {
+        return ` <div class="dashboard">
+                   <div class="content__top-panel">
+                     <h2 class="page_title">Dashboard</h2>
+                     <div data-element="RangePicker"></div>
+                   </div>
+                   <div data-element="ChartsRoot" class="dashboard__charts">
+                      <div data-element="ordersChart" class="dashboard__chart_orders"></div>
+                      <div data-element="salesChart" class="dashboard__chart_sales"></div>
+                      <div data-element="customersChart" class="dashboard__chart_customers"></div>
+                   </div>
+                   <h3 class="block-title" >Best sales</h3>
+                   <div data-element="SortableTable"></div>
+                </div>
+               `
+    }
+     render() {
+        this.element = document.createElement('div'); // (*)
+        this.element.innerHTML = this.template;
+        this.element = this.element.firstElementChild;
+        this.subElements = this.getSubElements(this.element);
+
+        this.initComponents();
+        this.randerComponents() ;
+        this.initEventListener();
+     }
+
+     getSubElements(element) {
+         const elements = element.querySelectorAll('[data-element]');
+
+         return [...elements].reduce((accum, subElement) => {
+          accum[subElement.dataset.element] = subElement;
+
+          return accum;
+        }, {});
+     }
+
+     initEventListener() {
+        this.components.RangePicker.dispatchEvent('data-select', event => {   //  //this.components.RangePicker.addEventListener('data-select', event => { 
+            const {from, to } = event.detal;
+            this.updateComponents(from, to);
+        });
+     }
+
+
+      remove () {
+        if (this.element) {
+            this.element.remove();
+        }
+     }
+
+      // NOTE: удаляем обработчики событий, если они есть
+      destroy() {
+        this.remove();
+        for(let component in this.components) {
+            this.components[component].destroy();
+        }
+        this.element = null;
+        this.subElements = {};
+      }
+
 
 }
